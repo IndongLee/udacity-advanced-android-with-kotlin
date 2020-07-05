92123eeb3f156a6ccefcfce3273a50896d33460d
IndongLee <kundera@kakao.com>
2020-07-05 22:11:30 +0900

    Write First Test

---
+++ 09_Android_Testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,36 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+        val tasks = listOf<Task>(
+                Task("title", "description", isCompleted = false)
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(0f, result.completedTasksPercent)
+        assertEquals(100f, result.activeTasksPercent)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsFortySixty() {
+        val tasks = listOf<Task>(
+                Task("title", "description", isCompleted = true),
+                Task("title", "description", isCompleted = true),
+                Task("title", "description", isCompleted = false),
+                Task("title", "description", isCompleted = false),
+                Task("title", "description", isCompleted = false)
+        )
+
+        val result = getActiveAndCompletedStats(tasks)
+
+        assertEquals(40f, result.completedTasksPercent)
+        assertEquals(60f, result.activeTasksPercent)
+    }
+}
