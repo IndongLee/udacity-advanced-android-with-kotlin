8d5a46265b16ec30c210decef5ab6a003343b9b1
IndongLee <kundera@kakao.com>
2020-07-06 23:34:38 +0900

    Add hamcrest

--- 09_Android_Testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ 09_Android_Testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,20 +1,26 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
 import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.*
 import org.junit.Test
 
 class StatisticsUtilsTest {
 
+    // subjectUnderTest_actionOrInput_resultState()
     @Test
     fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+        // GIVEN a list of tasks with a single, active, task
         val tasks = listOf<Task>(
                 Task("title", "description", isCompleted = false)
         )
 
+        // WHEN you call getActiveAndCompleteStats
         val result = getActiveAndCompletedStats(tasks)
 
+        // THEN there are 0% completed tasks and 100% active tasks
         assertEquals(0f, result.completedTasksPercent)
+        assertThat(result.completedTasksPercent, `is`(0f))
         assertEquals(100f, result.activeTasksPercent)
     }
 
