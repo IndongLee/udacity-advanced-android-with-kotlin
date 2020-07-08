56a7b72d095143b90e0fa8ea3d2703791fba5aae
IndongLee <kundera@kakao.com>
2020-07-08 22:50:31 +0900

    Set up a ViewModel Test

---
+++ 09_Android_Testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.example.android.architecture.blueprints.todoapp.tasks
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TasksViewModelTest {
+
+    @Test
+    fun addNewTask_setsNewTaskEvent() {
+        // Given a fresh TasksViewModel
+        val taskViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+
+        // When adding a new task
+        taskViewModel.addNewTask()
+
+
+        // Then the new task event is triggered
+    }
+}
